e575c77218a400cb4f372e6e55219af93e37fa49
Ramiro Garcia <ramiro.jgmp@gmail.com>
2020-12-16 20:31:47 -0600

    Add default photos is no photos was found

--- legall_rimac_virtual/lib/repositories/inspections_repository.dart
+++ legall_rimac_virtual/lib/repositories/inspections_repository.dart
@@ -1,8 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:legall_rimac_virtual/models/models.dart';
+import 'dart:convert';
 
 class InspectionsRepository {
   final _inspectionCollection = FirebaseFirestore.instance.collection('inspections');
+  final _photosCollection = FirebaseFirestore.instance.collection('photos');
+  final _videosCollection = FirebaseFirestore.instance.collection('videos');
 
   Stream<InspectionModel> get(String inspectionId) {
     return _inspectionCollection
@@ -20,6 +24,53 @@ class InspectionsRepository {
     }
     else {
       var doc = inspections.docs.first;
+      try {
+        //Load default resources
+        var resDefault = json.decode(await rootBundle.loadString('assets/default.json')) as Map<String,dynamic>;
+
+        if (resDefault.containsKey('photos')) {
+          //Check for empty photos
+          var photos = await _photosCollection
+              .where('inspection_id',isEqualTo: doc.id)
+              .limit(1)
+              .get();
+
+          if (photos.docs.isEmpty) {
+            //The inspection is empty at photos add by default
+            for(Map<String,dynamic> json in resDefault['photos'] as List<dynamic>) {
+              var photo = PhotoModel.fromJSON(json);
+              photo.inspectionId = doc.id;
+              photo.creator = PhotoCreator.inspector;
+              photo.helpText = photo.helpText??photo.description;
+              photo.status = ResourceStatus.empty;
+              photo.dateTime = DateTime.now();
+              await _photosCollection.add(photo.toJSON());
+            }
+          }
+        }
+
+        if (resDefault.containsKey('videos')) {
+          //Check for empty photos
+          var videos = await _videosCollection
+              .where('inspection_id',isEqualTo: doc.id)
+              .limit(1)
+              .get();
+
+          if (videos.docs.isEmpty) {
+            //The inspection is empty at videos add by default
+            for(Map<String,dynamic> json in resDefault['videos'] as List<dynamic>) {
+              var video = VideoModel.fromJSON(json);
+              video.inspectionId = doc.id;
+              video.helpText = video.helpText??video.description;
+              video.status = ResourceStatus.empty;
+              video.dateTime = DateTime.now();
+              await _videosCollection.add(video.toJSON());
+            }
+          }
+        }
+      } catch(ex) {
+       print(ex.toString());
+      }
       return InspectionModel.fromJSON(doc.data(),id: doc.id);
     }
   }
